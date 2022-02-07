ebf1eed4a8bc735e17f200fe80f79bed3d05a27a
ajoy267 <andrew.joy12@hotmail.com>
2022-02-07 13:01:46 -0800

    used user context state

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -3,8 +3,8 @@ import { createContext, useContext, useState } from 'react'
 const UserContext = createContext()
 
 function UserProvider({ children }) {
-  const [user, setUser] = useState('')
-  return <UserContext.Provider value={({ user }, { setUser })}>{children}</UserContext.Provider>
+  const [user, setUser] = useState({})
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>
 }
 
 function useUser() {
